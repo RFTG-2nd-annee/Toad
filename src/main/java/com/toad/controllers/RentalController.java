6dea0ab5ce41d45b1bf6e15fc4e23d5c6affcf4f
RoxasYTB <roxasyotabe@gmail.com>
2025-01-31 11:19:40 +0100

    Maj Rental

--- src/main/java/com/toad/controllers/RentalController.java
+++ src/main/java/com/toad/controllers/RentalController.java
@@ -1,5 +1,8 @@
 package com.toad.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.toad.entities.Film;
 import com.toad.entities.Rental;
+import com.toad.repositories.FilmRepository;
 import com.toad.repositories.RentalRepository;
 
 @Controller // This means that this class is a Controller
@@ -19,6 +24,9 @@ import com.toad.repositories.RentalRepository;
 public class RentalController {
     @Autowired
     private RentalRepository RentalRepository; // Assuming you have a RentalRepository for Rental entity
+
+    @Autowired
+    private FilmRepository FilmRepository;
 
     @PutMapping(path = "/update/{id}") // Map ONLY PUT Requests for updating a film
     public @ResponseBody String updateRental(
@@ -57,7 +65,7 @@ public class RentalController {
             @RequestParam String last_update) {
     
         Rental newRental = new Rental();
-        newRental.setRentalDate(rental_date);
+        newRental.setRentalDate(java.time.LocalDateTime.now().toString());
         newRental.setInventoryId(inventory_id);
         newRental.setCustomerId(customer_id);
         newRental.setReturnDate(return_date);
@@ -100,5 +108,30 @@ public class RentalController {
             return filteredRental;
         }
         return null;
+    }
+
+    @GetMapping(path = "/cart")
+    public @ResponseBody HashMap<String, Object> getCart() {
+        Iterable<Rental> rentals = RentalRepository.findAll();
+        Iterable<Film> films = FilmRepository.findAll();
+        //splice by 5
+        ArrayList<Object> cart = new ArrayList<>();
+        for (Rental rental : rentals) {
+            HashMap<String, Object> cartItem = new HashMap<>();
+            cartItem.put("rental_date", rental.getRentalDate());
+            cartItem.put("rental_id", rental.getRentalId());
+
+            Film film = films.iterator().next();
+            if (film != null) {
+                cartItem.put("film_title", film.getTitle());
+            }
+            // cartItem.put("customer_name", rental.getCustomerName());
+            // cartItem.put("inventory_id", rental.getInventoryId());
+            cart.add(cartItem);
+        }
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("rentals", cart);
+        return response;
     }
 }
